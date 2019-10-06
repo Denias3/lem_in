a9808cde3d4c13b62c0dfd815f5a8343e7e2d2fe
emeha <balakin.denis7@gmail.com>
2019-10-06 14:03:47 +0300

    добавил карту

--- algorithm.c
+++ algorithm.c
@@ -333,7 +333,7 @@ void			algorithm(t_anthill *ant, t_room *rooms)
 
 	while (to_position(rooms) == 0 && (p_ways = possible_ways(rooms)) > 0)
 	{
-//		print_rooms(rooms, 0);
+		print_rooms(rooms, 0);
 		del_copies(rooms, ant);
 		if (short_way(rooms, ant) == 0)
 		{
